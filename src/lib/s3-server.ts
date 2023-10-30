3346214c050fdb45ba5e020aefcd7d61d513c536
experience happiness <47346770+zhouyang-bigdata@users.noreply.github.com>
2023-10-30 13:44:33 +0800

    Update s3-server.ts

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -4,7 +4,7 @@ export async function downloadFromS3(file_key: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
       const s3 = new S3({
-        region: "ap-southeast-1",
+        region: "us-east-2",
         credentials: {
           accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
           secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
