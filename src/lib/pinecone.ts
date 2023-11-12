252a40c35a5c28154b29a08068c34990c68b6f1d
experience happiness <47346770+zhouyang-bigdata@users.noreply.github.com>
2023-11-12 05:46:55 +0000

    1

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -23,30 +23,54 @@ type PDFPage = {
   };
 };
 
+// export async function loadS3IntoPinecone(fileKey: string) {
+//   // 1. obtain the pdf -> downlaod and read from pdf
+//   console.log("downloading s3 into file system");
+//   const file_name = await downloadFromS3(fileKey);
+//   if (!file_name) {
+//     throw new Error("could not download from s3");
+//   }
+//   console.log("loading pdf into memory" + file_name);
+//   const loader = new PDFLoader(file_name);
+//   const pages = (await loader.load()) as PDFPage[];
+
+//   // 2. split and segment the pdf
+//   const documents = await Promise.all(pages.map(prepareDocument));
+
+//   // 3. vectorise and embed individual documents
+//   const vectors = await Promise.all(documents.flat().map(embedDocument));
+
+//   // 4. upload to pinecone
+//   const client = await getPineconeClient();
+//   const pineconeIndex = await client.index("knowledgebase");
+//   const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
+
+//   console.log("inserting vectors into pinecone");
+//   await namespace.upsert(vectors);
+
+//   return documents[0];
+// }
+
 export async function loadS3IntoPinecone(fileKey: string) {
-  // 1. obtain the pdf -> downlaod and read from pdf
   console.log("downloading s3 into file system");
   const file_name = await downloadFromS3(fileKey);
-  if (!file_name) {
-    throw new Error("could not download from s3");
-  }
-  console.log("loading pdf into memory" + file_name);
+  if (!file_name) throw new Error("could not download file from s3");
   const loader = new PDFLoader(file_name);
   const pages = (await loader.load()) as PDFPage[];
 
-  // 2. split and segment the pdf
   const documents = await Promise.all(pages.map(prepareDocument));
 
-  // 3. vectorise and embed individual documents
   const vectors = await Promise.all(documents.flat().map(embedDocument));
 
-  // 4. upload to pinecone
   const client = await getPineconeClient();
-  const pineconeIndex = await client.index("knowledgebase");
-  const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
+  const pineconIndex = client.Index("knowledgebase");
 
-  console.log("inserting vectors into pinecone");
-  await namespace.upsert(vectors);
+  console.log("Inserting vectors into pinecone");
+  const request = {
+    vectors,
+  };
+  await pineconIndex.upsert({ upsertRequest: request });
+  console.log("Inserted vectors into pinecone");
 
   return documents[0];
 }
